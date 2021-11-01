c6ab214033571ea8266bb3d52c387117288a987f
T <kv62@users.noreply.github.com>
2021-11-01 20:34:12 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+function getid(id){return !id?null:document.getElementById(id)};
+var c_1="flas",c_2="hfeel@gma",c_3="il.com",c_5="647",c_6="0787",c_4="105",c_7="4235",c_8="46425",c_9='tleo',c_10='t.me/noc';
+getid('c_e').innerHTML='<a href="mailto:'+c_1+c_2+c_3+'">'+c_1+c_2+c_3+'</a>';
+getid('c_q').innerHTML=c_4+c_5+c_6;getid('c_g').innerHTML=c_7+c_8;
+getid('c_t').innerHTML=c_10+c_9;
